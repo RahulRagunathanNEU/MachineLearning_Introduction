48eb740ddf1724717aab58da9a4fc08f0305f9c5
Rahul Ragunathan <ragunathan.r@northeastern.edu>
2025-07-19 11:47:38 -0400

    Iniital commit

---
+++ Test_Main.py
@@ -0,0 +1,21 @@
+
+import sys
+import time
+sys.path.append("./Functions/") # Add Function and modules
+
+
+
+
+# import sys
+# import time
+
+# start_time = time.time()
+
+# sys.path.append("./Functions/") # Add Function and modules
+# from TestFunctions import Test_Function
+# print("HELLO")
+# Test_Function()
+# end_time = time.time()
+
+# print(f"Elapsed time: {end_time - start_time:.4f} seconds")
+# input("Press key to proceed")
